155307d0e29068cbd4c29c16aaf6efd70d0027e8
Jose Antonio Delgado Alfonso <jdelgadoalfonso@openmailbox.org>
2018-12-19 10:04:41 +0100

    the feature  has been stable since 1.26.0 and no longer requires an attribute to enable

--- src/lib.rs
+++ src/lib.rs
@@ -30,8 +30,6 @@
 //!    message. Rust doesn't have a convenient way to only treat *some* of a buffer as utf-8,
 //!    so I'm just not supporting that. Most "real" syslog servers barf on it anway.
 //!
-#![feature(match_default_bindings)]
-
 #[cfg(test)]
 extern crate assert_matches;
 extern crate serde;
